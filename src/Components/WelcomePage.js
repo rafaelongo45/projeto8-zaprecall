cb42f0100b44c9022a54b6fde58d26da184d8d74
Rafael <rafaelongo45@gmail.com>
2022-03-16 01:13:55 -0300

    Primeiro commit. Estilização da tela de boas vindas feita

---
+++ src/Components/WelcomePage.js
@@ -0,0 +1,14 @@
+function WelcomePage() {
+    return (
+        <main className="welcome-page">
+            <section className="header">
+                <img src="./Assets/img/logo.png" alt="Page logo"></img>
+                <h1>ZapRecall</h1>
+            </section>
+            
+            <button className="start-quizz">Iniciar Recall!</button>
+        </main>
+    )
+}
+
+export default WelcomePage;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import reactDom from "react-dom";
+import WelcomePage from "./Components/WelcomePage";
+
+function App(){
+    return(
+        <WelcomePage />
+    )
+}
+
+const root = document.querySelector('.root');
+reactDom.render(<App/>, root);
